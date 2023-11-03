440840fbca6267819777772999fef83001b9ad21
Spector3702 <overfly3702@gmail.com>
2023-11-03 21:54:22 +0800

    readd the port os env

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import tensorflow as tf
 
@@ -87,4 +88,5 @@ def get_SOX_today():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
