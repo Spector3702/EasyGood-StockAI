8d2f02075325d80d060eb74e436f6c7ba0df3560
Wojolan <109399275+aa901002x@users.noreply.github.com>
2024-06-09 22:26:10 +0800

    Fix: 修改新聞輸出格式化

--- server/geminiBot.py
+++ server/geminiBot.py
@@ -1,4 +1,3 @@
-import sys
 import google.generativeai as genai
 from firebase import firebase
 from linebot import LineBotApi
@@ -19,6 +18,27 @@ class GeminiBot():
         
         self.handle_event(event)
 
+    def generate_news(self):
+        # 使用 Gemini AI 生成新聞標題和連結
+        prompt = ("請提供我最新最重要的三個財金新聞(只需要標題、不需要內文)，並且附上新聞的連結，中文\n"
+                  "各網域連結請依照下面範例:\n"
+                  "ET : https://finance.ettoday.net/news/\n"
+                  "經濟日報 : https://money.udn.com/money/story\n"
+                  "MoneyDJ : https://www.moneydj.com/funddj\n"
+                  "上述都是從這網址中抓新聞的URL前綴字語，只需要在後面加上其對應的新聞頁面即可")
+        messages = [{'role': 'user', 'parts': [prompt]}]
+        response = self.model.generate_content(messages)
+        
+        news_list = response.text.strip().split('\n')
+        formatted_news = "\n".join([f"{news.strip()}" for news in news_list if news.strip()])     
+        
+        # 移除Markdown格式
+        formatted_news = formatted_news.replace('**', '').replace('- [連結]', '').replace('(', ' ').replace(')', '')
+        
+        return formatted_news
+
+
+
     def handle_event(self, event):
         msg_type = event.message.type
         reply_token = event.reply_token
@@ -28,7 +48,10 @@ class GeminiBot():
         if isinstance(event.message, TextMessage):
             user_message = event.message.text
             if user_message == '推播新聞':
-                user_message = '請提供我最新最重要的三個財金新聞(只需要標題、不需要內文)，並且附上新聞的連結，中文'
+                news_content = self.generate_news()
+                reply_msg = TextSendMessage(text=news_content)
+                self.line_bot_api.reply_message(reply_token, reply_msg)
+                return
             
             if chatgpt is None:
                 messages = []
@@ -45,7 +68,7 @@ class GeminiBot():
                 reply_msg = TextSendMessage(text=response.text)
                 if event.message.text != '推播新聞':
                     # 更新 firebase 中的對話紀錄
-                    self.fdb.put_async(self.user_chat_path, None, messages)
+                    self.fdb.put(self.user_chat_path, None, messages)
             
             # 回覆訊息
             self.line_bot_api.reply_message(reply_token, reply_msg)
